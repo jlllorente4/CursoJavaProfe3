c644ff66ed4c2cb6d7b8bb257cabe3aba6849b72
josel <josel@JOSE-MSI>
2024-09-27 13:19:37 +0200

    Colecciones parte I

---
+++ src/es/cursojava/colecciones/ClaseInterfaz.java
@@ -0,0 +1,19 @@
+package es.cursojava.colecciones;
+
+import es.cursojava.poo.Persona;
+
+public class ClaseInterfaz extends Persona implements Interfaz1{
+
+	@Override
+	public void metodo1() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void metodo2() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/es/cursojava/colecciones/Interfaz1.java
@@ -0,0 +1,9 @@
+package es.cursojava.colecciones;
+
+public interface Interfaz1 {
+
+	public void metodo1();
+	
+	public void metodo2();
+	
+}
